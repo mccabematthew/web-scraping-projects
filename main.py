12ce58c60529b5d99a75ff22898475465e56eb74
Matth <matthew.james.mccabe@outlook.com>
2023-03-19 00:30:02 -0500

    parsed html file for h2 tags

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('my_website.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
+
+    soup = BeautifulSoup(content, 'lxml')
+    tags = soup.find_all('h2')
+    print(tags)
